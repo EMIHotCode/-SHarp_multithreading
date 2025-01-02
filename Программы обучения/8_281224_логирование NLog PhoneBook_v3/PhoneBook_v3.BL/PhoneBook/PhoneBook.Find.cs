ab560cd1edef6f28a65cb82a8463c88de68ebddc
EMIHotCode <163670522+EMIHotCode@users.noreply.github.com>
2025-01-02 06:16:57 +0300

    010125

---
+++ Программы обучения/8_281224_логирование NLog PhoneBook_v3/PhoneBook_v3.BL/PhoneBook/PhoneBook.Find.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using PhoneBook_v3.BL.Models;
+
+namespace PhoneBook_v3.BL;
+
+public partial class PhoneBook
+{
+    public IEnumerable<Contact>? FindAllByName(string name)
+    {
+        var filter = Builders<Contact>.Filter.Eq(p=>p.Name, name);
+        return _collection.Find(filter).ToList();
+    }
+    
+    public IEnumerable<Contact>? FindAllByPhone(string number)
+    {
+        var filter = Builders<Contact>.Filter.ElemMatch("Phones", Builders<Phone>.Filter.Eq(p=>p.Number, number));
+        return _collection.Find(filter).ToList();
+    }
+    
+    public IEnumerable<Contact>? FindAllByComment(string comment)
+    {
+        var filter = Builders<Contact>.Filter.ElemMatch("Phones", Builders<Phone>.Filter.Eq(p=>p.Comment, comment));
+        return _collection.Find(filter).ToList();
+    }
+
+    public IEnumerable<Contact>? GetAll()
+    {
+        try
+        {
+            return _collection.Find(new BsonDocument()).ToList();
+        }
+        catch (Exception e)
+        {
+            Logger.Fatal(e, "Ошибка получения данных из БД");
+            throw;
+        }
+        
+    }
+}
